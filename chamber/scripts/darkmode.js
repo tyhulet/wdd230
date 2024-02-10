fbb0093d5499e290fa6e076f43f7f9b5cca5106c
TyHulet <tyhulet@byui.edu>
2024-02-09 20:48:04 -0700

    chamber update css and js

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -2,8 +2,7 @@ const modeButton = document.querySelector("#mode");
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 const body = document.querySelector("body");
-
-
+const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("Darkmode")) {
@@ -11,14 +10,14 @@ modeButton.addEventListener("click", () => {
         header.style.color = "#fff";
         footer.style.background = "#32AE99";
         footer.style.color = "#000000";
-        body.style.background = "#000000";
+        main.style.background = "#000000";
 
         modeButton.textContent = "LightMode";
     } else {
         header.style.background = "#32AE99";
         header.style.color = "#000000";
-        body.style.background = "lightgray";
-        body.style.color = "#000000";
+        main.style.background = "lightgray";
+        main.style.color = "#000000";
 
         modeButton.textContent = "Darkmode";
     }
