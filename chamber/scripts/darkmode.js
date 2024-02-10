b89f677adb1ded4b2b9602ec1b99cb3f14f47971
TyHulet <tyhulet@byui.edu>
2024-02-10 10:15:18 -0700

    Chamber dark mode update

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -5,20 +5,20 @@ const body = document.querySelector("body");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("Darkmode")) {
+    if (modeButton.textContent.includes("DarkMode 🌕")) {
         header.style.background = "#000000";
         header.style.color = "#fff";
         footer.style.background = "#32AE99";
         footer.style.color = "#000000";
         main.style.background = "#000000";
 
-        modeButton.textContent = "LightMode";
+        modeButton.textContent = "LightMode 🌞";
     } else {
         header.style.background = "#32AE99";
         header.style.color = "#000000";
         main.style.background = "lightgray";
         main.style.color = "#000000";
 
-        modeButton.textContent = "Darkmode";
+        modeButton.textContent = "DarkMode 🌕";
     }
 });
