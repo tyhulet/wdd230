b72e1e04e82f41cb6a8d84e9c3ff0c146aee5654
TyHulet <tyhulet@byui.edu>
2024-02-15 20:28:53 -0700

    chamber and homepage update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,15 @@
-document.getElementById('windchill').textContent = document.windchill;
-
-
-function windchill(temp, wind) {
-    const temp = document.getElementById("temp");
-    const wind = document.getElementById('wind');
-    const f = 35.74 + 0.6215 * temp - 35.75 * wind ** 0.16 + 0.4275 * temp * wind ** 0.16
-    if (temp <= 50 && speed > 3) {
-        windchill = f.toFixed(1) + "°F";
+function doInputOutput() {
+    let temperature = 45;
+    let windspeed = 10;
+    if (temperature <= 50 && windspeed >= 3) {
+        output = windChill(temperature, windspeed);
+    } else {
+        output = `N/A`
     }
-    else {
-        windchill = "NA";
-    }
-    return windchill;
-};
+    document.getElementById('windchill').innerHTML = "Wind Chill: ${output}";
+}
 
+function windChill(tempF, speed) {
+    let f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+    return f;
+}
