0e2e33aa2efd480256a5548c8ee8b04d850335c4
TyHulet <tyhulet@byui.edu>
2024-02-21 10:12:59 -0700

    homepage update

---
+++ scripts/visits.js
@@ -0,0 +1,15 @@
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = 'This your first visit. Welcome!';
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+
