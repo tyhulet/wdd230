fe436cb3f5fd10800482467f7ba47c99f5941900
TyHulet <tyhulet@byui.edu>
2024-03-06 19:59:14 -0700

    homepage update

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,29 @@
 const baseURL = "https://tyhulet.github.io/wdd230/";
-const linksURl = "https://tyhulet.github.io/wdd230/data/links.json";
+const linksURL = "https://tyhulet.github.io/wdd230/data/links.json";
+
+const lists = document.querySelector("#links")
 
 async function getlinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
 }
 
 getlinks();
+
+
+const displayLinks = (lessons) => {
+    lessons.forEach((weeks) => {
+        const list = document.createElement('li');
+        list.textContent = `Week ${weeks.lesson}: `;
+
+        weeks.links.forEach(week => {
+            const anchorTag = document.createElement('a');
+            anchorTag.href = `${week.url}`;
+            anchorTag.textContent = `${week.title} `;
+            list.appendChild(anchorTag);
+        });
+        lists.appendChild(list);
+
+    });
+}
