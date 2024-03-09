bc81f9c57d7d981427819abe5a2ef61d6f009074
TyHulet <tyhulet@byui.edu>
2024-03-09 11:27:18 -0700

    directory update check

--- chamber/scripts/member.js
+++ chamber/scripts/member.js
@@ -45,4 +45,12 @@ function displayProphets(companies) {
     });
 }
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
 
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
