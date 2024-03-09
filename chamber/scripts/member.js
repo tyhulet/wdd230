43f21b6dbcb63b1f0fa6e28447d0fc5ee2a671f1
TyHulet <tyhulet@byui.edu>
2024-03-09 12:34:33 -0700

    css update

--- chamber/scripts/member.js
+++ chamber/scripts/member.js
@@ -50,18 +50,4 @@ function displayProphets(companies) {
 
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-gridbutton.addEventListener("click", () => {
-    // example using arrow function
-    display.classList.add("grid");
-    display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-    display.classList.add("list");
-    display.classList.remove("grid");
-}
 
