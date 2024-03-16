3e2e7df3e99a6c00ba25a90df2e9b0bfae6c76c6
TyHulet <tyhulet@byui.edu>
2024-03-15 21:43:58 -0600

    coc homepage update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,10 +2,17 @@ const currentTemp = document.querySelector('#temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const forecastTemp1 = document.querySelector('#forecast-temp-1');
-const weatherIcon1 = document.querySelector('#weather-icon1');
-const captionDesc1 = document.querySelector('#fig2');
+const Temp1 = document.querySelector("#temp-1");
+const Icon1 = document.querySelector("#icon1");
+const Desc1 = document.querySelector("#fig1");
 
+const Temp2 = document.querySelector("#temp-2");
+const Icon2 = document.querySelector("#icon2");
+const Desc2 = document.querySelector("#fig2");
+
+const Temp3 = document.querySelector("#temp-3");
+const Icon3 = document.querySelector("#icon3");
+const Desc3 = document.querySelector("#fig3");
 
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.04&lon=-111.73&units=imperial&appid=d17732e032e0814a027db79d84ba388e'
@@ -43,8 +50,8 @@ async function forecastapiFetch() {
         const response = await fetch(url1);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
-            displayResults(data);
+            console.log(data); //Testing only
+            displayForecast(data); // uncomment when ready
         } else {
             throw Error(await response.text());
         }
@@ -53,18 +60,33 @@ async function forecastapiFetch() {
     }
 }
 
-forecastapiFetch();
+function displayForecast(data) {
+    Temp1.innerHTML = `${data.list[6].main.temp.toFixed(1)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.list[6].weather[0].icon}.png`;
+    let desc1 = data.list[6].weather[0].description;
+    Icon1.setAttribute("src", iconsrc);
+    Icon1.setAttribute("alt", "weather icon");
+    Desc1.textContent = `${desc1}`;
 
-function displayforeCast(data) {
-    forecastTemp1.innerHTML = `${data.list[6].main.temp.tofixed(1)}&deg;F`;
-    const inconsrc = `https://openweathermap.org/img/w/${data.list[6].weather[0].icon}.png`;
-    let desc1 = data.list[6]
+    Temp2.innerHTML = `${data.list[14].main.temp.toFixed(1)}&deg;F`;
+    const iconsrc1 = `https://openweathermap.org/img/w/${data.list[14].weather[0].icon}.png`;
+    let desc2 = data.list[14].weather[0].description;
+    Icon2.setAttribute("src", iconsrc1);
+    Icon2.setAttribute("alt", "weather icon");
+    Desc2.textContent = `${desc2}`;
+
+    Temp3.innerHTML = `${data.list[22].main.temp.toFixed(0)}&deg;F`;
+    const iconsrc2 = `https://openweathermap.org/img/w/${data.list[22].weather[0].icon}.png`;
+    let desc3 = data.list[22].weather[0].description;
+    Icon3.setAttribute("src", iconsrc2);
+    Icon3.setAttribute("alt", "weather icon");
+    Desc3.textContent = `${desc3}`;
 }
 
 
 
 
 
-
+forecastapiFetch();
 
 
