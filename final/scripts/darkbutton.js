e1f0094923f61945cbf2d97565da367c108f6101
TyHulet <tyhulet@byui.edu>
2024-04-09 12:28:48 -0600

    final update

---
+++ final/scripts/darkbutton.js
@@ -0,0 +1,24 @@
+const modeButton = document.querySelector("#mode");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("DarkMode 🌕")) {
+        header.style.background = "#000000";
+        header.style.color = "#fff";
+        footer.style.background = "#32AE99";
+        footer.style.color = "#000000";
+        main.style.background = "#000000";
+
+        modeButton.textContent = "LightMode 🌞";
+    } else {
+        header.style.background = "#32AE99";
+        header.style.color = "#000000";
+        main.style.background = "lightgray";
+        main.style.color = "#000000";
+
+        modeButton.textContent = "DarkMode 🌕";
+    }
+});
