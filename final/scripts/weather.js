5a418f7ca47a96b06b32d6626b78f391c3e296c9
TyHulet <tyhulet@byui.edu>
2024-04-10 21:14:00 -0600

    final finshed

--- final/scripts/weather.js
+++ final/scripts/weather.js
@@ -1,5 +1,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
+const humidity = document.querySelector('#humidity');
+const hightemp = document.querySelector('#high');
 const captionDesc = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=20.51&lon=-89.95&units=imperial&appid=d17732e032e0814a027db79d84ba388e'
@@ -23,6 +25,8 @@ apiFetch();
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp.toFixed(1)}&deg;F`;
+    humidity.innerHTML = `${data.main.humidity}%`;
+    hightemp.innerHTML = `${data.main.temp_max.toFixed(1)}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
